d98b62d75f48722235e8f2bf30516a6ca94b7418
stuartcatt <168027816+stuartcatt@users.noreply.github.com>
2024-05-16 12:44:21 -0700

    Update main.js

--- new-tab/main.js
+++ new-tab/main.js
@@ -7,7 +7,7 @@ import { BackgroundController } from "./background-controller.js";
 
 const WIFI_URL = "chrome://network/#select";
 const BLUETOOTH_URL = "chrome://bluetooth-pairing";
-const SETTINGS_URL = "chrome://settings";
+const SETTINGS_URL = "chrome://os-settings";
 const NEW_TAB_URL = "chrome://new-tab-page";
 const FILES_URL = "chrome://file-manager";
 const HELP_URL = "https://github.com/bypassiwastaken/skiovox-helper";
